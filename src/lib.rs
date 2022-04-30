fcdec68faee7e938e9aebf98f2d70bfea45f6ab6
Eugene <jewgeni@protonmail.com>
2022-04-30 22:57:23 +0200

    tests && more APIs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,26 @@
 #![allow(
     clippy::uninit_assumed_init,
     clippy::mem_replace_with_uninit,
-    dead_code
+    dead_code,
+    soft_unstable
 )]
 #![feature(
     const_trait_impl,
     const_mut_refs,
     const_ptr_is_null,
+    const_default_impls,
     allocator_api,
+    maybe_uninit_uninit_array,
     const_replace,
     const_try,
     type_ascription,
     inline_const,
+    test,
+    bench_black_box,
     const_ptr_offset_from,
     const_ptr_write,
     const_ptr_read,
     const_intrinsic_copy
 )]
 
-pub mod map;
 pub mod stack;
--- src/stack.rs
+++ src/stack.rs
@@ -1,22 +1,51 @@
+extern crate alloc;
 extern crate crossbeam;
+use core::{ptr, sync::atomic::AtomicUsize, sync::atomic::Ordering};
 use crossbeam::{
     epoch::{self, Atomic, Owned},
     utils::CachePadded,
 };
-use std::{ptr, sync::atomic::Ordering};
 
 #[derive(Debug)]
 pub struct Stack<T> {
     top: Atomic<CachePadded<Node<T>>>,
+    size: AtomicUsize,
 }
 
 impl<T> Stack<T> {
+    /// Create a new empty `Stack<T>`.
+    ///
+    /// # Examples
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let s = Stack::<()>::new();
+    ///
+    /// assert!(s.is_empty());
+    /// ```
+    ///
+    #[inline(always)]
     pub const fn new() -> Self {
         Self {
             top: Atomic::null(),
+            size: Default::default(),
         }
     }
+
+    /// Pushes a value onto a stack.
+    ///
+    /// # Examples
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let mut s = Stack::<i32>::new();
+    ///
+    /// s.push(1);
+    /// assert_eq!(s.pop(), Some(1));
+    /// ```
+    #[inline(always)]
     pub fn push(&self, val: T) {
+        self.size.fetch_add(1, Ordering::Relaxed);
         let mut n = Owned::new(CachePadded::new(Node {
             val,
             next: Atomic::null(),
@@ -24,8 +53,8 @@ impl<T> Stack<T> {
         let g = epoch::pin();
 
         loop {
-            let ptr = self.top.load(Ordering::Relaxed, &g);
-            n.next.store(ptr, Ordering::Relaxed);
+            let ptr = self.top.load(Ordering::Acquire, &g);
+            n.next.store(ptr, Ordering::Release);
             match self
                 .top
                 .compare_exchange(ptr, n, Ordering::Release, Ordering::Relaxed, &g)
@@ -36,7 +65,19 @@ impl<T> Stack<T> {
         }
     }
 
-    #[allow(deprecated)]
+    /// Pops the element from the top of the stack.
+    ///
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let s = Stack::new();
+    ///
+    /// s.push(1);
+    ///
+    /// assert_eq!(s.pop(), Some(1));
+    /// assert_eq!(s.pop(), None);
+    /// ```
+    #[inline(always)]
     pub fn pop(&self) -> Option<T> {
         let g = epoch::pin();
         loop {
@@ -50,8 +91,8 @@ impl<T> Stack<T> {
                         .is_ok()
                     {
                         unsafe {
-                            g.defer_unchecked(move || ptr.into_owned());
-                            return Some(ptr::read(&(*head).val));
+                            self.size.fetch_sub(1, Ordering::Relaxed);
+                            break Some(ptr::read(&(*head).val));
                         }
                     }
                 }
@@ -59,23 +100,118 @@ impl<T> Stack<T> {
             }
         }
     }
+
+    /// Returns the number of elements in the stack.
+    ///
+    /// # Examples
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let s = Stack::new();
+    ///
+    /// assert_eq!(s.len(), 0);
+    ///
+    /// s.push(1);
+    ///
+    /// assert_eq!(s.len(), 1);
+    /// ```
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.size.load(Ordering::Relaxed)
+    }
+
+    /// Returns `true` if the stack contains no elements.
+    ///
+    /// # Examples
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let s = Stack::new();
+    ///
+    /// assert!(s.is_empty());
+    ///
+    /// s.push(1);
+    ///
+    /// assert!(!s.is_empty());
+    /// ```
+    #[inline(always)]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    /// Extends the stack with the given iterable. Acts just like
+    /// [`Extend::extend`](https://doc.rust-lang.org/std/iter/trait.Extend.html#method.extend)`
+    /// but doesn't require mutability.
+    ///
+    /// # Examples
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let mut s = Stack::new();
+    ///
+    /// s.extend(1..=3);
+    ///
+    /// assert_eq!(s.pop(), Some(3));
+    /// assert_eq!(s.pop(), Some(2));
+    /// assert_eq!(s.pop(), Some(1));
+    /// assert_eq!(s.pop(), None);
+    /// ```
+    #[inline]
+    pub fn extend<I>(&self, iter: I)
+    where
+        I: IntoIterator<Item = T>,
+    {
+        for val in iter {
+            self.push(val);
+        }
+    }
+
+    /// Clears the stack, removing all elements.
+    ///
+    /// # Examples
+    /// ```
+    /// use lockfreed::stack::Stack;
+    ///
+    /// let mut s = Stack::new();
+    ///
+    /// s.push(1);
+    /// s.push(2);
+    ///
+    /// assert_eq!(s.len(), 2);
+    ///
+    /// s.clear();
+    ///
+    /// assert_eq!(s.len(), 0);
+    /// ```
+    #[inline(always)]
+    pub fn clear(&self) {
+        while self.pop().is_some() {}
+    }
 }
 
-impl<T> const Default for Stack<T> {
+impl<T> Default for Stack<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
+impl<T> Drop for Stack<T> {
+    fn drop(&mut self) {
+        self.clear();
+    }
+}
+
 #[derive(Debug)]
-pub struct Node<T> {
+struct Node<T> {
     val: T,
     next: Atomic<CachePadded<Node<T>>>,
 }
 
 #[cfg(test)]
 mod tests {
+    extern crate test;
     use super::*;
+    use std::sync::Arc;
     #[test]
     fn test_stack() {
         let s = Stack::new();
@@ -86,5 +222,74 @@ mod tests {
         assert_eq!(s.pop(), Some(2));
         assert_eq!(s.pop(), Some(1));
         assert_eq!(s.pop(), None);
+    }
+
+    #[test]
+    fn test_multithread_stack() {
+        let s = Arc::new(Stack::new());
+
+        let mut handles = Vec::with_capacity(8);
+        for _ in 0..8 {
+            handles.push(std::thread::spawn({
+                let s = s.clone();
+                move || {
+                    for i in 0..100 {
+                        s.push(i * 2);
+                        s.push(i * 4);
+                        s.pop();
+                    }
+                }
+            }));
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        assert_eq!(s.len(), 800);
+        let mut left = 0;
+        while let Some(_) = s.pop() {
+            left += 1;
+        }
+        assert_eq!(left, 800);
+        assert_eq!(s.len(), 0);
+    }
+
+    #[test]
+    fn no_data_corruption() {
+        const NTHREAD: usize = 20;
+        const NITER: usize = 800;
+        const NMOD: usize = 55;
+
+        let stack = Arc::new(Stack::new());
+        let mut handles = Vec::with_capacity(NTHREAD);
+
+        for i in 0..NTHREAD {
+            let stack = stack.clone();
+            handles.push(std::thread::spawn(move || {
+                for j in 0..NITER {
+                    let val = (i * NITER) + j;
+                    stack.push(val);
+                    if (val + 1) % NMOD == 0 {
+                        if let Some(val) = stack.pop() {
+                            assert!(val < NITER * NTHREAD);
+                        }
+                    }
+                }
+            }));
+        }
+
+        for handle in handles {
+            handle.join().expect("thread failed");
+        }
+
+        let expected = NITER * NTHREAD - NITER * NTHREAD / NMOD;
+        let mut res = 0;
+        while let Some(val) = stack.pop() {
+            assert!(val < NITER * NTHREAD);
+            res += 1;
+        }
+
+        assert_eq!(res, expected);
     }
 }
